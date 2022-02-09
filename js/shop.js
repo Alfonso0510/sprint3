ec3f321751f4c69ed5ca15c8b36c4e4f33b801c1
Alfonso Vacas <alfonsovacas1979@gmail.com>
2022-02-09 18:34:54 +0100

    Ejercicio 1

--- js/shop.js
+++ js/shop.js
@@ -65,8 +65,13 @@ var total = 0;
 
 // Exercise 1
 function buy(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+    for (let i = 0; i < products.length; i++) {
+        if (id == products[i].id) {
+            cartList.push(products[i]);
+        }
+    }
+    console.log(cartList)
+    generateCart();
 }
 
 // Exercise 2
